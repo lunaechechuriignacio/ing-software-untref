d701d45f4a84239c3dec1b3cf645a43f9571b75d
ignaciolunaechechuri <lunaechechri.ignacio@gmail.com>
2015-10-12 20:08:35 -0300

    Commit se agrego nuevos escenarios de DispararBarcosStepdefs

--- Ejercicio3/src/test/java/skeleton/DispararBarcosStepdefs.java
+++ Ejercicio3/src/test/java/skeleton/DispararBarcosStepdefs.java
@@ -8,8 +8,7 @@ import cucumber.api.java.en.Then;
 
 public class DispararBarcosStepdefs {
 	private BatallaNaval batallaNaval;
-	
-	@Given("^no hay barcos en posicion (\\d+),(\\d+)$")
+		@Given("^no hay barcos en posicion (\\d+),(\\d+)$")
 	public void no_hay_barcos_en_posicion(int arg1, int arg2) throws Throwable {
 	   batallaNaval=new BatallaNaval(10);
 	}
@@ -22,5 +21,18 @@ public class DispararBarcosStepdefs {
 	@Then("^el disparo dio en el agua$")
 	public void el_disparo_dio_en_el_agua() throws Throwable {
 	   Assert.assertFalse(batallaNaval.isEstadoDisparoABarco());
+	}
+	
+	@Given("^hay un barco en posicion (\\d+),(\\d+)$")
+	public void hay_un_barco_en_posicion(int fila, int columna) throws Throwable {
+		String tipoBarco = "Lancha";
+		String orientacionBarco = "Horizontal";
+		batallaNaval = new BatallaNaval(10);
+		batallaNaval.ubicarBarco(fila, columna, tipoBarco, orientacionBarco);
+	}
+
+	@Then("^el disparo dio en el blanco$")
+	public void el_disparo_dio_en_el_blanco() throws Throwable {
+		Assert.assertTrue(batallaNaval.isEstadoDisparoABarco());
 	}
 }
--- Ejercicio3/src/test/resources/skeleton/DispararBarcos.feature
+++ Ejercicio3/src/test/resources/skeleton/DispararBarcos.feature
@@ -5,4 +5,9 @@ Feature: Como usuario quiero disparar a los barcos enemigos
     When disparo a la posicion 1,8
     Then el disparo dio en el agua
 
+  Scenario: Disparo y toco un barco pero no lo hundo
+    Given hay un barco en posicion 1,8
+    When disparo a la posicion 1,8
+    Then el disparo dio en el blanco
+
  

e4a5584ad0197bd92254c4ad7bbb1989d1dd27be
ignaciolunaechechuri <lunaechechri.ignacio@gmail.com>
2015-10-11 22:01:03 -0300

    Commit Subo la estructura del ejercicio3

---
+++ Ejercicio3/src/test/java/skeleton/DispararBarcosStepdefs.java
@@ -0,0 +1,66 @@
+package skeleton;
+
+import org.junit.Assert;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+public class DispararBarcosStepdefs {
+/*
+	@Given("^no hay barcos en posicion (\\d+),(\\d+)$")
+	public void no_hay_barcos_en_posicion(int arg1, int arg2) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^disparo a la posicion (\\d+),(\\d+)$")
+	public void disparo_a_la_posicion(int arg1, int arg2) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^el disparo dio en el agua$")
+	public void el_disparo_dio_en_el_agua() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^hay un barco en posicion (\\d+),(\\d+)$")
+	public void hay_un_barco_en_posicion(int arg1, int arg2) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^el disparo dio en el blanco$")
+	public void el_disparo_dio_en_el_blanco() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^disparo a la posicion (\\d+),(\\d+) And el disparo dio en el blanco$")
+	public void disparo_a_la_posicion_And_el_disparo_dio_en_el_blanco(int arg1,
+			int arg2) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^todas las posiciones del barco han sido destruidas$")
+	public void todas_las_posiciones_del_barco_han_sido_destruidas()
+			throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^el barco ha sido hundido$")
+	public void el_barco_ha_sido_hundido() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^no se puede disparar a esa posicion$")
+	public void no_se_puede_disparar_a_esa_posicion() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+*/
+}
---
+++ Ejercicio3/src/test/java/skeleton/UbicarBarcosStepdefs.java
@@ -0,0 +1,66 @@
+package skeleton;
+
+import org.junit.Assert;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+public class UbicarBarcosStepdefs {
+	/*
+	@Given("^posicion \\((\\d+),(\\d+)\\) esta libre y posicion \\((\\d+),(\\d+)\\) esta libre$")
+	public void posicion_esta_libre_y_posicion_esta_libre(int arg1, int arg2, int arg3, int arg4) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^jugador posiciona un \"(.*?)\" en la posicion \\((\\d+),(\\d+)\\) \"(.*?)\"$")
+	public void jugador_posiciona_un_en_la_posicion(String arg1, int arg2, int arg3, String arg4) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^barco posicionado exitosamente$")
+	public void barco_posicionado_exitosamente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^hay un barco en posicion \\((\\d+),(\\d+)\\)$")
+	public void hay_un_barco_en_posicion(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^jugador posiciona un barco en la posicion \\((\\d+),(\\d+)\\) \"(.*?)\"$")
+	public void jugador_posiciona_un_barco_en_la_posicion(int arg1, int arg2, String arg3) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^posicion ocupada no se puede ubicar el barco alli$")
+	public void posicion_ocupada_no_se_puede_ubicar_el_barco_alli() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^jugador posiciona un  \"(.*?)\" en la posicion \\((\\d+),(\\d+)\\) \"(.*?)\"$")
+	public void jugador_posiciona_un_en_la_posicion(String arg1, int arg2, int arg3, String arg4) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^tablero es de (\\d+)x(\\d+)$")
+	public void tablero_es_de_x(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^la posicion elegida no pertenece al tablero$")
+	public void la_posicion_elegida_no_pertenece_al_tablero() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+*/
+}
+
